4418c5dd1f0a86999b28ae9e8da499b3a2660b92
Jeremy LAURENT <jeremy.laurent6@gmail.com>
2017-11-28 18:09:34 +0100

    Ajout navigation sidebar 2

--- gabarit.php
+++ gabarit.php
@@ -55,7 +55,6 @@
                 </ul>
 
                 <ul class="list-unstyled CTAs">
-                    <li><a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a></li>
                     <li><a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a></li>
                 </ul>
             </nav>
