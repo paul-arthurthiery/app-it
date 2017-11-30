3575e186a268c94348cc26d44fe6eda22bd41959
paul-arthurthiery <paul-arthur.thiery@isep.fr>
2017-11-30 16:54:33 +0100

    Added name to Appartment table (for real this time)

--- db_smartPanel.sql
+++ db_smartPanel.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 29, 2017 at 03:51 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Host: localhost:8889
+-- Generation Time: Nov 30, 2017 at 04:52 PM
+-- Server version: 5.6.28
+-- PHP Version: 7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `smartpanel`
@@ -28,14 +20,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `appartment`
 --
 
-DROP TABLE IF EXISTS `appartment`;
-CREATE TABLE IF NOT EXISTS `appartment` (
+CREATE TABLE `appartment` (
+  `Name` varchar(20) NOT NULL,
   `ApptId` int(11) NOT NULL,
   `Address` varchar(255) NOT NULL,
   `NumberRooms` tinyint(4) NOT NULL,
-  `User_Id` int(11) NOT NULL,
-  PRIMARY KEY (`ApptId`),
-  KEY `fk_UserID` (`User_Id`)
+  `User_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -44,13 +34,10 @@ CREATE TABLE IF NOT EXISTS `appartment` (
 -- Table structure for table `room`
 --
 
-DROP TABLE IF EXISTS `room`;
-CREATE TABLE IF NOT EXISTS `room` (
+CREATE TABLE `room` (
   `RoomID` tinyint(4) NOT NULL,
   `ApartmentID` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  PRIMARY KEY (`RoomID`),
-  KEY `fk_ApartmentID` (`ApartmentID`)
+  `Name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -59,15 +46,12 @@ CREATE TABLE IF NOT EXISTS `room` (
 -- Table structure for table `sensor`
 --
 
-DROP TABLE IF EXISTS `sensor`;
-CREATE TABLE IF NOT EXISTS `sensor` (
+CREATE TABLE `sensor` (
   `SensorID` tinyint(4) NOT NULL,
   `Type` varchar(50) NOT NULL,
   `Value` varchar(20) NOT NULL,
   `RoomID` tinyint(4) NOT NULL,
-  `IsActuator` tinyint(1) NOT NULL,
-  PRIMARY KEY (`SensorID`),
-  KEY `fk_RoomID` (`RoomID`)
+  `IsActuator` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -76,14 +60,12 @@ CREATE TABLE IF NOT EXISTS `sensor` (
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `User_Id` int(11) NOT NULL,
   `Username` varchar(20) NOT NULL,
   `Password` varchar(32) NOT NULL,
   `FullName` varchar(50) NOT NULL,
-  `IsAdmin` tinyint(1) NOT NULL,
-  PRIMARY KEY (`User_Id`)
+  `IsAdmin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -93,6 +75,74 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`User_Id`, `Username`, `Password`, `FullName`, `IsAdmin`) VALUES
 (123456789, 'Jean-mich', '63a9f0ea7bb98050796b649e85481845', 'Jean-Michel', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `identifiant` text COLLATE utf8_unicode_ci NOT NULL,
+  `mdp` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `identifiant`, `mdp`) VALUES
+(1, 'matthieu', '202cb962ac59075b964b07152d234b70'),
+(2, 'raja', '202cb962ac59075b964b07152d234b70'),
+(3, 'zakia', '202cb962ac59075b964b07152d234b70'),
+(4, 'jeremy', '202cb962ac59075b964b07152d234b70');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appartment`
+--
+ALTER TABLE `appartment`
+  ADD PRIMARY KEY (`ApptId`),
+  ADD KEY `fk_UserID` (`User_Id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`RoomID`),
+  ADD KEY `fk_ApartmentID` (`ApartmentID`);
+
+--
+-- Indexes for table `sensor`
+--
+ALTER TABLE `sensor`
+  ADD PRIMARY KEY (`SensorID`),
+  ADD KEY `fk_RoomID` (`RoomID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_Id`);
+
+--
+-- Indexes for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -114,8 +164,3 @@ ALTER TABLE `room`
 --
 ALTER TABLE `sensor`
   ADD CONSTRAINT `fk_RoomID` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
