0d776a8184e66aed98e711f4568792f5cb16a5e2
paul-arthurthiery <paul-arthur.thiery@isep.fr>
2017-11-30 16:22:30 +0100

    New way to add HTML to php vars + start of creat appartment page

--- Vue/connexion_erreur.php
+++ Vue/connexion_erreur.php
@@ -1,6 +1,7 @@
 <?php
-    $erreur = '<div class="alert alert-danger" role="alert" style="text-align : center;" id="shaker">';
-    $erreur .= 'Identifiant ou mot de passe incorect';
-    $erreur .= '</div>';
+    $erreur = '
+	    <div class="alert alert-danger" role="alert" style="text-align : center;" id="shaker">
+	    Identifiant ou mot de passe incorect
+	    </div>';
     include ('Vue/non_connecte.php');
     ?>
--- Vue/create_appartment.php
+++ Vue/create_appartment.php
@@ -1,3 +1,15 @@
 <?php
-	 
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Create Appartment page</title>
+	</head>
+	<body>
+		<form action="create_appartment_submit" method="get" accept-charset="utf-8">
+			Nom: <input type="text" name="Name">
+			
+		</form>
+	</body>
+</html>
