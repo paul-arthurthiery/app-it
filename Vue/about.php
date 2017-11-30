bf773244aac9860cedc89e2aaba0c5f44e4f87e9
Adrien Mauduit <adrienmauduit@MacBook-Pro-de-Adrien-2.local>
2017-11-30 17:14:06 +0100

    test page about adrien

--- Vue/about.php
+++ Vue/about.php
@@ -4,7 +4,48 @@ $contenu = '
 
 
 success
+<!DOCTYPE html>
+<html>
+	<head>
 
+
+
+
+
+	</head>
+
+
+	<body>
+
+	<h1 class="titre" >About us </h1>
+	<p>This is a paragraph.so if i just rwigth stuff like that verry verry long <br> so we break line bishy bishy <br> 
+	<img src="doggo.gif" alt="dog on tutle" style="width:150px;height:149px;">
+
+	</p>
+	<p>other stuff to say in an another paragraph 
+	<a href="https://www.w3schools.com">This is a link</a>
+	and continue  </p>
+	<a href="https://www.w3schools.com">This is a link</a>
+
+	<hr>
+
+	<h2 id="dog-smilling">This is a Heading</h2>
+	<p>This is a paragraph.so if i just rwigth stuff like that </p>
+	<p>other stuff to say in an another paragraph </p>
+	<img src="dog.jpg" alt="this is dog" width="200" height="200">
+
+	<hr>
+
+	<h3 class="titre">I am dog </h3>
+	<p style="color:blue">I am a paragraph</p>
+	<p>This is a paragraph.so if i just rwigth stuff like that<br>
+	<iframe width="420" height="315"
+src="https://www.youtube.com/embed/O6P2_i0Y6ms">
+</iframe> </p>
+	<p>other stuff to say in an another paragraph </p>
+
+	</body>
+</html>
 
 
 ';
