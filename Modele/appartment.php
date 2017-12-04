88855f00ab000fedb7108592ed672c5b0f77635a
Alexis Menantaud <alexismenantaud1@gmail.com>
2017-12-04 15:59:42 +0100

    Modification DB + query création appartement (auto increment de l'appt ID)

--- Modele/appartment.php
+++ Modele/appartment.php
@@ -2,7 +2,7 @@
     require("connexion.php");
 
     function createapt($db,$name,$address,$nbrRooms,$User_Id){
-      $db->query('INSERT INTO appartment (Name,ApptId,Address,NumberRooms,User_Id) VALUES  ("'.$name.'", 3, "'.$address.'", "'.$nbrRooms.'", "'.$User_Id.'")');
+      $db->query('INSERT INTO appartment (Name,Address,NumberRooms,User_Id) VALUES  ("'.$name.'", "'.$address.'", "'.$nbrRooms.'", "'.$User_Id.'")');
     }
 
 
--- db_smartPanel.sql
+++ db_smartPanel.sql
@@ -1,183 +1,77 @@
--- phpMyAdmin SQL Dump
--- version 4.7.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: Dec 04, 2017 at 10:24 AM
--- Server version: 5.6.35
--- PHP Version: 7.1.8
-
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
---
--- Database: `smartpanel`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+DROP DATABASE IF EXISTS `smartpanel`;
 CREATE DATABASE IF NOT EXISTS `smartpanel` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `smartpanel`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `appartment`
---
-
 DROP TABLE IF EXISTS `appartment`;
-CREATE TABLE `appartment` (
+CREATE TABLE IF NOT EXISTS `appartment` (
   `Name` varchar(20) NOT NULL,
-  `ApptId` int(11) NOT NULL,
+  `ApptId` int(11) NOT NULL AUTO_INCREMENT,
   `Address` varchar(255) NOT NULL,
   `NumberRooms` tinyint(4) NOT NULL,
-  `User_Id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `appartment`
---
+  `User_Id` int(11) NOT NULL,
+  PRIMARY KEY (`ApptId`),
+  KEY `fk_UserID` (`User_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 INSERT INTO `appartment` (`Name`, `ApptId`, `Address`, `NumberRooms`, `User_Id`) VALUES
 ('Apt de michel', 1, '1 rue de Paris', 3, 123456789),
 ('Apt de john', 2, '2 rue des Pins', 4, 2);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `room`
---
-
 DROP TABLE IF EXISTS `room`;
-CREATE TABLE `room` (
+CREATE TABLE IF NOT EXISTS `room` (
   `RoomID` tinyint(4) NOT NULL,
   `ApartmentID` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL
+  `Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`RoomID`),
+  KEY `fk_ApartmentID` (`ApartmentID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `sensor`
---
-
 DROP TABLE IF EXISTS `sensor`;
-CREATE TABLE `sensor` (
+CREATE TABLE IF NOT EXISTS `sensor` (
   `SensorID` tinyint(4) NOT NULL,
   `Type` varchar(50) NOT NULL,
   `Value` varchar(20) NOT NULL,
   `RoomID` tinyint(4) NOT NULL,
-  `IsActuator` tinyint(1) NOT NULL
+  `IsActuator` tinyint(1) NOT NULL,
+  PRIMARY KEY (`SensorID`),
+  KEY `fk_RoomID` (`RoomID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `User_Id` int(11) NOT NULL,
   `Username` varchar(20) NOT NULL,
   `Password` varchar(32) NOT NULL,
   `FullName` varchar(50) NOT NULL,
-  `IsAdmin` tinyint(1) NOT NULL
+  `IsAdmin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`User_Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user`
---
 
 INSERT INTO `user` (`User_Id`, `Username`, `Password`, `FullName`, `IsAdmin`) VALUES
 (2, 'jony', '63a9f0ea7bb98050796b649e85481845', 'John Doe', 0),
 (3, 'alexis', '63a9f0ea7bb98050796b649e85481845', 'Alexis', 0),
 (123456789, 'Jean-mich', '63a9f0ea7bb98050796b649e85481845', 'Jean-Michel', 1);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `utilisateurs`
---
-
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE `utilisateurs` (
-  `id` int(11) NOT NULL,
-  `identifiant` text COLLATE utf8_unicode_ci NOT NULL,
-  `mdp` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `utilisateurs`
---
-
-INSERT INTO `utilisateurs` (`id`, `identifiant`, `mdp`) VALUES
-(1, 'matthieu', '202cb962ac59075b964b07152d234b70'),
-(2, 'raja', '202cb962ac59075b964b07152d234b70'),
-(3, 'zakia', '202cb962ac59075b964b07152d234b70'),
-(4, 'jeremy', '202cb962ac59075b964b07152d234b70');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `appartment`
---
-ALTER TABLE `appartment`
-  ADD PRIMARY KEY (`ApptId`),
-  ADD KEY `fk_UserID` (`User_Id`);
-
---
--- Indexes for table `room`
---
-ALTER TABLE `room`
-  ADD PRIMARY KEY (`RoomID`),
-  ADD KEY `fk_ApartmentID` (`ApartmentID`);
-
---
--- Indexes for table `sensor`
---
-ALTER TABLE `sensor`
-  ADD PRIMARY KEY (`SensorID`),
-  ADD KEY `fk_RoomID` (`RoomID`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`User_Id`);
-
---
--- Indexes for table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `appartment`
---
 ALTER TABLE `appartment`
   ADD CONSTRAINT `fk_UserID` FOREIGN KEY (`User_Id`) REFERENCES `user` (`User_Id`);
 
---
--- Constraints for table `room`
---
-ALTER TABLE `room`
-  ADD CONSTRAINT `fk_ApartmentID` FOREIGN KEY (`ApartmentID`) REFERENCES `appartment` (`ApptId`);
-
---
--- Constraints for table `sensor`
---
 ALTER TABLE `sensor`
   ADD CONSTRAINT `fk_RoomID` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
