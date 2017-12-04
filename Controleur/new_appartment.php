8f81f38dae213f0f59770af8e2ce7e70ea991daa
Alexis Menantaud <alexismenantaud1@gmail.com>
2017-12-04 11:13:03 +0100

    Revert "Fixed bug where the apt creation page always appeared"
    This reverts commit 269d8616237461367cb3032105ca740593a50440.

--- Controleur/new_appartment.php
+++ Controleur/new_appartment.php
@@ -1,14 +1,14 @@
 <?php
 	if(isset($_GET['cible']) && $_GET['cible']=="createapt") {
-		if(!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['nbrRooms'])){
+		if(!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['mdp'])){
 			include("Model/appartment.php");
 
 
 			//do the queries plz
 		} else {
-			$erreur = '<p>Veuillez remplir tous les champs</p>';
-			include("Vue/create_appartment.php");
+			$erreur = "Veuillez remplir tous les champs";
 		}
+	} else {
+		include("Vue/create_appartment.php");
 	}
-
 ?>
