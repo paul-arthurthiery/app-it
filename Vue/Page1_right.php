4852c862c9a24bc564cd418c42d7af9d38af04f0
ZhaojiaLii <ZhaojiaLii@users.noreply.github.com>
2017-12-04 09:02:46 +0100

    Page1_right

---
+++ Vue/Page1_right.php
@@ -0,0 +1,18 @@
+<?php
+  
+  $contenu = '
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<title></title>
+  </head>
+  <body>
+     <p> This is Page1 </p>
+  
+  </body>
+  </html>
+
+  '
+  include('gabarit.php');
+  ?>
+  
