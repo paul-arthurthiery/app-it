59acb04bccbe81b510b2b4e62cf5e7400fbfe139
Alexis Menantaud <alexismenantaud1@gmail.com>
2017-12-05 18:23:29 +0100

    Test affichage liste utilisateurs dac accueil.php (pour menu navigation)

--- Modele/utilisateurs.php
+++ Modele/utilisateurs.php
@@ -9,7 +9,7 @@
 
     function appt($db,$User_Id){
 
-        $reponse = $db->query('SELECT * FROM (SELECT ApptId,appartment.User_Id FROM appartment INNER JOIN user ON user.User_Id=appartment.User_Id) AS tempo WHERE User_Id="'.$User_Id.'"');
+        $reponse = $db->query('SELECT * FROM (SELECT Name, ApptId, appartment.User_Id FROM appartment INNER JOIN user ON user.User_Id=appartment.User_Id) AS tempo WHERE User_Id="'.$User_Id.'"');
         return $reponse;
     }
 
--- Vue/accueil.php
+++ Vue/accueil.php
@@ -1,8 +1,26 @@
 <?php
     $entete = entete("Mon site / Accueil");
     $menu = menu("accueil");
-    $contenu = "<h2>Contenu de l'accueil</h2>";
+
+
+
+    //include("Modele/utilisateurs.php");
+    $appt = appt($db,$_SESSION['User_Id']);
+    //$reponse_appt = $appt->fetchAll(PDO::FETCH_ASSOC);
+    //$reponse_appt_name=$reponse_appt['Name'];
+    //$contenu = $reponse_appt_name;
+    $contenu = '<ul>';
+    $reponse_appt=$appt->fetchAll();
+    for ($i = 0; $i<count($reponse_appt); $i++) {
+      //$contenu .= '<li> <a href="index.php?cible=controleurAppt&'".$reponse_appt['ApptId']."'">'".$reponse_appt['Name']."' </a></li>';
+      //$valueTest= $currentAppt['Name'];
+      $data=$reponse_appt[$i]['Name'];
+      $contenu .="<li>$data</li>";
+    }
+    $contenu .= '</ul>';
+
+
     $pied = pied();
 
+
     include 'gabarit.php';
-?>
