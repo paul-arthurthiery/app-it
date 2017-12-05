565a51b129d6685e75c2e27adcd843fcf41ea224
ZhaojiaLii <ZhaojiaLii@users.noreply.github.com>
2017-12-05 14:06:46 +0100

    My profile Page

--- Vue/Myprofile.php
+++ Vue/Myprofile.php
@@ -4,19 +4,6 @@ $contenu = '
 
 
 
-<!DOCTYPE html>
-<html>
-  <head>
-
-
-  </head>
-
-
-  <body>
-     <p> This is My Profile. </p>
-
-  </body>
-</html>
 
 
 ';
@@ -24,3 +11,36 @@ $contenu = '
 include('gabarit.php')
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>My Profile Page</title>
+	<style type="text/css">
+
+
+		
+	</style>
+</head>
+<body>
+	<h1 class="h1"> My Profile</h1>
+	<hr/>
+	<div class="container">
+		<div class="profiles">
+			<form>
+			    <font size="4">First Name :</font><input type="text" name="firstname"  value="1" readonly="true"> <br/>
+                <font size="4">Last Name : </font> <input type="text" name="lastname" value="2" readonly="true"> <br/>
+                <font size="4">User ID : </font><input type="text" name="userid" value="3" readonly="true"> 
+                <font size="4">Appartment ID :</font><input type="text" name="appid" value="4" readonly="true"> <br/>
+                <font size="4">Address : </font><input type="text" name="address" value="5" readonly="true"> 
+                <font size="4">Postal code : </font><input type="text" name="code" value="6" readonly="true">			
+			</form>
+           
+
+			
+		</div>
+	</div>
+
+</body>
+</html>
