ac7444ed527071591c32039bb635bdde110c0720
Jeremy LAURENT <jeremy.laurent6@gmail.com>
2017-12-19 15:23:43 +0100

    proto page type capteur pour alexis

---
+++ Vue/administration/typesCapteurs.php
@@ -0,0 +1,98 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<style>
+table {
+        width: 1em;
+    }
+
+thead, tbody, tr, td, th { display: block; }
+
+tr:after {
+    content: ' ';
+    display: block;
+    visibility: hidden;
+    clear: both;
+}
+
+thead th {
+    /*height: 30px;
+
+    /*text-align: left;*/
+}
+
+tbody {
+    height: 400px;
+    overflow-y: auto;
+}
+
+thead {
+    /* fallback */
+}
+
+
+tbody td, thead th {
+    width:50%;
+    float: left;
+}
+</style>
+
+<div class = "container-fluid">
+  <h2>Types de capteurs</h2>
+<div class="span3">
+<table class="table table-striped">
+<thead>
+ <tr>
+   <th>#</th>
+   <th>Nom</th>
+ </tr>
+</thead>
+<tbody>
+ <tr>
+   <td>1</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>2</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>3</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>4</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>5</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>6</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>6</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>6</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>6</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>6</td>
+   <td>Something</td>
+ </tr>
+ <tr>
+   <td>6</td>
+   <td>Something</td>
+ </tr>
+</tbody>
+</table>
+
+</div>
+</div>
