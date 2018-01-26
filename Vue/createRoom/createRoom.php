ad78eff2338f745e073983c7cae6ad40b1bdeba6
Jeremy LAURENT <jeremy.laurent6@gmail.com>
2018-01-26 19:51:46 +0100

    uncrustify la page createroom despi

--- Vue/createRoom/createRoom.php
+++ Vue/createRoom/createRoom.php
@@ -6,9 +6,10 @@ $contenu = '
 <script src="jQuery/jquery-1.12.0.min.js"></script>
 <link rel="stylesheet" type="text/css" href="Vue/createRoom/createRoom.css">
 <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
-
+<div class="room-wall">
+<span id="center">
 <h1> Créer une pièce</h1>
-
+<br/>
 <form action="index.php?cible=postRooms&aptID='.$currentAptID.'" method="post">
 <input name="input_field[]" type="text" placeholder="Nom de la pièce">
 <div class="field_wrapper">
@@ -21,9 +22,11 @@ $contenu = '
 <a href="javascript:void(0);" class="add_input_button" title="Add field"><i class="fas fa-plus-circle fa-lg" height="20" width="20"></i></a>
 </div>
 </div>
-<input type="submit" name="save" value="Submit"/>
+<br/>
+<button class="btn btn-primary" type="submit" name="save">Créer la pièce</button>
 </form>
-
+</span>
+</div>
 <script src="Vue/createRoom/createRoom.js"> </script>
 
 ';
